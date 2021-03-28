54159608e095fe01e029fdce6c708b3743cd5882
mizhuo <wulibin0809@gmail.com>
2021-03-28 22:48:06 +0800

    add seata-demo

---
+++ seata-demo-bank2/src/main/java/site/mizhuo/seatademobank2/config/DataSourceConfiguration.java
@@ -0,0 +1,37 @@
+package site.mizhuo.seatademobank2.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * @author mizhuo
+ */
+@Configuration
+public class DataSourceConfiguration {
+
+    private final ApplicationContext applicationContext;
+
+    public DataSourceConfiguration(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.remote-db")
+    public DataSource dataSource(){
+        return new DruidDataSource();
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSourceProxy(DataSource dataSource){
+        DataSourceProxy dataSourceProxy = new DataSourceProxy(dataSource);
+        return dataSourceProxy;
+    }
+}
