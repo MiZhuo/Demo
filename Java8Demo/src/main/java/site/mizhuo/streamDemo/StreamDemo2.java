68141d71e4964a249cec57fff3a2bb96104c6e59
mizhuo <wulibin0809@gmail.com>
2021-04-07 09:50:45 +0800

    add streamApiTest

--- Java8Demo/src/main/java/site/mizhuo/streamDemo/StreamDemo2.java
+++ Java8Demo/src/main/java/site/mizhuo/streamDemo/StreamDemo2.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
@@ -18,7 +19,7 @@ public class StreamDemo2 {
      * 练习
      */
     @Test
-    public void test11(){
+    public void test(){
         //1:给定一个数字列表,如何返回每个数字的平方
         List<Integer> list = Arrays.asList(1,2,3,4,5,6);
         list.stream()
@@ -53,14 +54,81 @@ public class StreamDemo2 {
     }
 
     //1:找出2013年发生的交易，并按金额从低到高排序。
+    @Test
+    public void test1(){
+        transactions.stream()
+                .filter(t -> t.getYear() == 2013)
+                .sorted(Comparator.comparingInt(Transaction::getValue))
+                .forEach(System.out::println);
+    }
     //2:交易员都在哪些城市工作过。
+    @Test
+    public void test2(){
+        transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getCity)
+                .distinct()
+                .forEach(System.out::println);
+    }
     //3:查找所有来自shanghai的交易员,并按姓名排序。
+    @Test
+    public void test3(){
+        transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(t -> t.getCity().equals("shanghai"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .distinct()
+                .forEach(System.out::println);
+    }
     //4:返回所有交易员的姓名字符串,按字母顺序排序。
+    @Test
+    public void test4(){
+        char[] chars = transactions.stream()
+                .map(Transaction::getTrader)
+                .map(Trader::getName)
+                .distinct()
+                .sorted(String::compareTo)
+                .reduce(String::concat)
+                .map(String::toCharArray)
+                .get();
+        System.out.println(chars);
+    }
     //5:有没有交易员在hangzhou工作过的。
+    @Test
+    public void test5(){
+        long hangzhou = transactions.stream()
+                .map(Transaction::getTrader)
+                .distinct()
+                .filter(t -> t.getCity().equals("hangzhou"))
+                .count();
+        System.out.println(hangzhou);
+    }
     //6:打印生活在shanghai的所有交易员的所有交易额
+    @Test
+    public void test6(){
+        transactions.stream()
+                .filter(t -> t.getTrader().getCity().equals("shanghai"))
+                .map(Transaction::getValue)
+                .forEach(System.out::println);
+    }
     //7:所有交易中,交易的最高金额是多少.
+    @Test
+    public void test7(){
+        Integer maxInteger = transactions.stream()
+                .map(Transaction::getValue)
+                .max(Integer::compareTo)
+                .get();
+        System.out.println(maxInteger);
+    }
     //8:找到交易最小的金额。
-
+    @Test
+    public void test8(){
+        Integer maxInteger = transactions.stream()
+                .map(Transaction::getValue)
+                .min(Integer::compareTo)
+                .get();
+        System.out.println(maxInteger);
+    }
 
 
 
